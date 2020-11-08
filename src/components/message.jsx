a3ef5d2bb30f8b1fc7ffc7dad411d63bdad50c1f
cu <harrierraum@hotmail.com>
2020-11-08 22:48:28 +0100

    colorize usernames

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
+const stringToColour = (str) => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = '#';
+  for (let i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xFF;
+    colour += (`00 ${value.toString(16)}`).substr(-2);
+  }
+  return colour;
+};
+
 const Message = (props) => {
   const { created_at, author, content } = props.message;
   const time = new Date(created_at).toLocaleTimeString();
@@ -7,7 +20,7 @@ const Message = (props) => {
   return (
     <div className="message-container">
       <i className="author">
-        <span>{author}</span>
+        <span style={{ color: stringToColour(author) }}>{author}</span>
         <small>{time}</small>
       </i>
       <p>{content}</p>
